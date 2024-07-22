7f57fdd7e59c67ca01b0aa0c4bdd7ef5c9a0abaa
matt <19mattd@gmail.com>
2024-07-22 14:41:10 -0400

    Update setupmongo.sh

--- setupmongo.sh
+++ setupmongo.sh
@@ -1,4 +1,4 @@
 # create user, set up replset, import sample data
 mongosh setupreplset.js
-curl -L -o sample-data 'https://drive.google.com/uc?export=download&id=1mlFYUbABFxfww8YeoLkX4Rpef9dMljU0
+curl -L -o sample-data 'https://drive.google.com/uc?export=download&id=1mlFYUbABFxfww8YeoLkX4Rpef9dMljU0'
 mongorestore --username admin --password <pwd> sample-data 
