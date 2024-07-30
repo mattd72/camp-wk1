e788a5b141c42d8d202982d12c278bb51c2ca750
matt <19mattd@gmail.com>
2024-07-30 14:14:51 -0400

    Update importdata.sh

--- importdata.sh
+++ importdata.sh
@@ -1,2 +1,3 @@
 curl -L -o sample-data 'https://drive.google.com/uc?export=download&id=1mlFYUbABFxfww8YeoLkX4Rpef9dMljU0'
+mv sample-data sample-data.bson
 mongorestore --username="admin" --password="" sample-data
